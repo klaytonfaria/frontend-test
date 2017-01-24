c712143bcebd9132d50bc40f9cc1c1f57360f42f
Klayton Faria <klayton.souza@gmail.com>
2017-01-24 01:11:14 -0200

    Add chat header

---
+++ src/components/partials/chat/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="chat-window-header">
+    <span className="chat-window-title">Vaga: desenvolvedor Front-end</span>
+    <div className="chat-window-tools">
+      <span className="icon minimize" />
+      <span className="icon close" />
+    </div>
+  </div>
+);
+
+export default Header;

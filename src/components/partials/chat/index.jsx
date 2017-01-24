0471acc2e4322478b4262b6c8427dba908e7f307
Klayton Faria <klayton.souza@gmail.com>
2017-01-24 01:14:12 -0200

    Implementing flux on chat index as a container

--- src/components/partials/chat/index.jsx
+++ src/components/partials/chat/index.jsx
@@ -1,4 +1,36 @@
+import React, { Component } from 'react';
+// Components
 import Chat from './chat';
+// Components actions
+import ChatActions from './flux/actions';
+import ChatStore from './flux/store';
+// Components styles
 import './chat.scss';
 
-export default Chat;
+const URL = '/static/json/talk.json';
+
+class ChatContainer extends Component {
+
+  componentWillMount() {
+    ChatStore.addChangeListener((chatData) => {
+      this.setState({
+        data: chatData
+      });
+    });
+
+    ChatActions.fetchAll(URL);
+  }
+
+  componentWillUnmount() {
+    ChatStore.removeChangeListener();
+  }
+
+  render() {
+    const data = this.state && this.state.data ? this.state.data : false;
+    return (
+      <Chat data={data} />
+    );
+  }
+}
+
+export default ChatContainer;
